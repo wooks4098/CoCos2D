33ce6da6c027e7a327aa0042343ff158400ee541
yuzi1125 <62372676+yuzi1125@users.noreply.github.com>
2021-05-17 11:43:46 +0900

    유닛 생성, 이동 및 충돌 테스트

--- Classes/TestScene1.cpp
+++ Classes/TestScene1.cpp
@@ -13,9 +13,79 @@ bool TestScene1::init()
     {
         return false;
     }
-    auto wlayer = LayerColor::create(Color4B::YELLOW);
+    auto wlayer = LayerColor::create(Color4B::GRAY);
     this->addChild(wlayer);
     
+    //유닛 생성
+    createUnit1();
+    createUnit2();
+
+    //유닛 충돌 체크
+    this->schedule(schedule_selector(TestScene1::collisionUnit1));
+    this->schedule(schedule_selector(TestScene1::collisionUnit2));
+
 
     return true;
+}
+
+//유닛 생성
+void TestScene1::createUnit1()
+{
+ 	//팩토리 위치로 수정하기
+    Size winSize = Director::getInstance()->getWinSize();
+	Vec2 factory1 = Vec2(0, winSize.height / 2);
+    Vec2 factory2 = Vec2(winSize.width, winSize.height / 2);
+
+    unit1 = Sprite::create("CyanSquare.png");
+    unit1->setPosition(factory1);
+    this->addChild(unit1);
+
+    //유닛 이동
+    moveUnit1 = MoveTo::create(3, factory2); //상대 팩토리로 이동
+    unit1->runAction(moveUnit1); //액션 실행
+}
+
+void TestScene1::createUnit2()
+{
+    //팩토리 위치로 수정하기
+    Size winSize = Director::getInstance()->getWinSize();
+    Vec2 factory1 = Vec2(0, winSize.height / 2);
+    Vec2 factory2 = Vec2(winSize.width, winSize.height / 2);
+
+    unit2 = Sprite::create("YellowSquare.png");
+    unit2->setPosition(factory2);
+    this->addChild(unit2);
+
+    //유닛 이동
+    moveUnit2 = MoveTo::create(3, factory1); //상대 팩토리로 이동
+    unit2->runAction(moveUnit2); //액션 실행
+}
+
+void TestScene1::collisionUnit1(float f)
+{
+    if (unit1->getBoundingBox().intersectsRect(unit2->getBoundingBox()))
+    {
+        log("unit1 unit2 collision");
+
+        fightUnit();
+    }
+}
+
+void TestScene1::collisionUnit2(float f)
+{
+    if (unit2->getBoundingBox().intersectsRect(unit1->getBoundingBox()))
+    {
+        log("unit2 unit1 collision");
+
+        fightUnit();
+    }
+}
+
+void TestScene1::fightUnit()
+{
+    this->stopAction(moveUnit1);
+    this->stopAction(moveUnit2);
+
+    log("unit fight!!! - %d", num);
+    num++;
 }
--- Classes/TestScene1.h
+++ Classes/TestScene1.h
@@ -3,6 +3,8 @@
 
 #include "cocos2d.h"
 
+using namespace cocos2d;
+
 class TestScene1 : public cocos2d::Scene
 {
 public:
@@ -12,7 +14,19 @@ public:
 
 	CREATE_FUNC(TestScene1);
 
-	
+
+	Sprite* unit1;
+	Sprite* unit2;
+	int num;
+	Action* moveUnit1;
+	Action* moveUnit2;
+
+
+	void createUnit1();
+	void createUnit2();
+	void collisionUnit1(float f);
+	void collisionUnit2(float f);
+	void fightUnit();
 };
 
 #endif // !__SceneTrans__TestScene1
