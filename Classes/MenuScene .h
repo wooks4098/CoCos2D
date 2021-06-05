3088fac27d1484f9032fe694009c42de43b67d2f
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-06-05 16:48:24 +0900

    메뉴 추가

--- Classes/MenuScene .h
+++ Classes/MenuScene .h
@@ -2,9 +2,14 @@
 #define __SceneTrans__MenuScene
 
 #include "cocos2d.h"
+using namespace cocos2d;
 
 class MenuScene : public cocos2d::Scene
 {
+private:
+	Size winSize;
+	Sprite* Image;
+	Menu *menu;
 public:
     static cocos2d::Scene* createScene();
 
@@ -13,6 +18,12 @@ public:
  
     CREATE_FUNC(MenuScene);
 
+	void CreatMenu();
+
+	//ฤÝน้
+	void Menu_Play(Ref* pSender);
+	void Menu_Option(Ref* pSender);
+	void Menu_Exit(Ref* pSender);
 
 };
 #endif // _SceneTrans__MenuScene
