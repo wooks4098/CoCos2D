20cf64540b5c318ff285d1537ffb1926c8e9b931
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-06-10 20:19:54 +0900

    SoundManager추가

--- Classes/SoundManager.cpp
+++ Classes/SoundManager.cpp
@@ -11,8 +11,63 @@ void SoundManager::Play(int _SoundNum)
 {
 	switch (_SoundNum)
 	{
-	case 0:
-		experimental::AudioEngine::play2d("Sounds/Test.ogg", false, 1.0f);
+	case Bubble_Creat:
+		experimental::AudioEngine::play2d("Sound/B_Create.wav", false, 1.0f);
+		break;
+	case Bubble_Mix:
+		experimental::AudioEngine::play2d("Sound/B_Mix.wav", false, 1.0f);
+		break;
+	case Bubble_Select:
+		experimental::AudioEngine::play2d("Sound/B_Select.wav", false, 1.0f);
+		break;
+	case Bubble_Sell:
+		experimental::AudioEngine::play2d("Sound/B_Sell.wav", false, 1.0f);
+		break;
+	case BackGround_0:
+		experimental::AudioEngine::play2d("Sound/BG00.wav", true, 1.0f);
+		break;
+	case BackGround_1:
+		experimental::AudioEngine::play2d("Sound/BG01.wav", false, 1.0f);
+		break;
+	case Bubble_Out:
+		experimental::AudioEngine::play2d("Sound/F_Bubbleout.wav", false, 1.0f);
+		break;
+	case Bubble_Set:
+		experimental::AudioEngine::play2d("Sound/F_Bubbleset.wav", false, 1.0f);
+		break;
+	case Factory_Damage:
+		experimental::AudioEngine::play2d("Sounds/BG00.mp3", false, 1.0f);
+		break;
+	case Unit_Attack_0:
+		experimental::AudioEngine::play2d("Sound/U_Attack00.wav", false, 1.0f);
+		break;
+	case Unit_Attack_1:
+		experimental::AudioEngine::play2d("Sound/U_Attack01.wav", false, 1.0f);
+		break;
+	case Unit_Attack_2:
+		experimental::AudioEngine::play2d("Sound/U_Attack02.wav", false, 1.0f);
+		break;
+	case Unit_Creat:
+		experimental::AudioEngine::play2d("Sound/U_Create.wav", false, 1.0f);
+		break;
+	case Unit_Dead_0:
+		experimental::AudioEngine::play2d("Sound/U_Dead00.wav", false, 1.0f);
+		break;
+	case Unit_Dead_1:
+		experimental::AudioEngine::play2d("Sound/U_Dead01.wav", false, 1.0f);
+		break;
+	case Unit_Dead_2:
+		experimental::AudioEngine::play2d("Sound/U_Dead02.wav", false, 1.0f);
+		break;
+	case Unit_Upgrade:
+		experimental::AudioEngine::play2d("Sound/U_Upgrade.wav", false, 1.0f);
+		break;
+	case UI_Click:
+		experimental::AudioEngine::play2d("Sound/UI_Click.wav", false, 1.0f);
+		break;
+	case UI_End:
+		experimental::AudioEngine::play2d("Sound/UI_End.wav", false, 1.0f);
+		break;
 
 	default:
 		break;
--- Classes/mecro.h
+++ Classes/mecro.h
@@ -26,6 +26,29 @@ enum Type
 	C1_Blue, C1_Red, C1_Yellow, R1_Blue, R1_Red, R1_Yellow, C2_Blue, C2_Red, C2_Yellow, R2_Blue, R2_Red, R2_Yellow, None_Circle, None_Rhombus
 };
 
+enum Sound
+{
+	Bubble_Creat = 0, 
+	Bubble_Mix, 
+	Bubble_Select , 
+	Bubble_Sell, 
+	BackGround_0, 
+	BackGround_1, 
+	Bubble_Out, 
+	Bubble_Set,
+	Factory_Damage,
+	Unit_Attack_0,
+	Unit_Attack_1,
+	Unit_Attack_2,
+	Unit_Creat,
+	Unit_Dead_0,
+	Unit_Dead_1,
+	Unit_Dead_2,
+	Unit_Upgrade,
+	UI_Click,
+	UI_End
+};
+
 class mecro
 {
 };
