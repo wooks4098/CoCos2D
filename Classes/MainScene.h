d0c5d221a408ba668dbbd4af3d3536e293c6e2cb
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-06-12 17:36:02 +0900

    메뉴 추가

---
+++ Classes/MainScene.h
@@ -0,0 +1,31 @@
+#ifndef __SceneTrans__MainScene
+#define __SceneTrans__MainScene
+#pragma once
+
+
+#include "mecro.h"
+
+
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class MainScene : public cocos2d::Scene
+{
+private:
+	Size winSize;
+	Sprite* Image;
+	Menu *menu;
+public:
+    static cocos2d::Scene* createScene();
+    virtual bool init();    
+    CREATE_FUNC(MainScene);
+	void CreatMenu();
+
+	//ฤÝน้
+	void Menu_Play(Ref* pSender);
+	void Menu_Option(Ref* pSender);
+	void Menu_Exit(Ref* pSender);
+};
+
+#endif // _SceneTrans__GameScene
